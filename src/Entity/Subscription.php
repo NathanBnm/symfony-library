11be4c06c309676b4ba3f83e7623bc9e6ebf8384
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2021-01-31 16:02:14 +0100

    Update project

---
+++ src/Entity/Subscription.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubscriptionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=SubscriptionRepository::class)
+ */
+class Subscription
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
